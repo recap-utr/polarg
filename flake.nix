161979b69415782301a04fe23bc710ea33208bad
Mirko Lenz <mirko@mirkolenz.com>
2023-06-18 21:16:39 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    poetry2nix,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {
+        pkgs,
+        lib,
+        system,
+        self',
+        ...
+      }: let
+        python = pkgs.python310;
+        poetry = pkgs.poetry;
+      in {
+        packages = {
+          default = poetry2nix.legacyPackages.${system}.mkPoetryApplication {
+            inherit python;
+            projectDir = ./.;
+            preferWheels = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = [poetry python];
+          POETRY_VIRTUALENVS_IN_PROJECT = true;
+          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc];
+          shellHook = ''
+            ${lib.getExe poetry} env use ${lib.getExe python}
+            ${lib.getExe poetry} install --all-extras --no-root
+          '';
+        };
+      };
+    };
+}
