7e509aec4dc4745c9a9507fac11f89b28a32cf7c
Mirko Lenz <mirko@mirkolenz.com>
2023-07-19 09:52:30 +0100

    feat: use wandb logger

--- argument_nli/train.py
+++ argument_nli/train.py
@@ -1,5 +1,6 @@
 import transformers.utils.logging as transformers_logging
 from lightning import Trainer
+from lightning.pytorch.loggers.wandb import WandbLogger
 
 from argument_nli.config import config
 from argument_nli.model import EntailmentDataModule, EntailmentModule
@@ -9,7 +10,17 @@ transformers_logging.set_verbosity_error()
 
 datamodule = EntailmentDataModule()
 
-trainer = Trainer(max_epochs=config.model.train.max_epochs)
+logger = WandbLogger(project="argument-nli")
+trainer = Trainer(
+    logger=logger,
+    max_epochs=config.model.train.max_epochs,
+    accelerator="gpu",
+    # strategy=DeepSpeedStrategy(
+    #     stage=3,
+    #     # offload_optimizer=True,
+    #     # offload_parameters=True,
+    # ),
+)
 model = EntailmentModule()
 
 trainer.fit(model, datamodule=datamodule)
